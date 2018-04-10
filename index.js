219c09bfdb8d6364878e579f53c5e304d1699652
Sundarasan <s.n.sundarasan@gmail.com>
2018-04-10 15:06:02 +0530

    Renames app name

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
 app.get('/', (req, res) => {
-  res.send('HEY!')
+  res.send('HEY! PM2 NODE SERVER')
 })
 app.listen(3000, () => console.log('Server running on port 3000'))
