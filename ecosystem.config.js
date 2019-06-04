77617d8059d0cd2dc0e330417ece1b151b0e6b53
Sundarasan <S.N.Sundarasan@gmail.com>
2019-06-04 15:21:25 +0530

    Updates repo name to cms

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   apps: [{
-    name: 'ec2-sample-express',
+    name: 'cms',
     script: './index.js'
   }],
   deploy: {
@@ -8,8 +8,8 @@ module.exports = {
       user: 'ubuntu',
       host: 'ec2-18-219-254-130.us-east-2.compute.amazonaws.com',
       ref: 'origin/master',
-      repo: 'git@github.com:Sundarasan/ec2-sample-express.git',
-      path: '/home/ubuntu/ec2-sample-express',
+      repo: 'git@github.com:Sundarasan/cms.git',
+      path: '/home/ubuntu/cms',
       'post-deploy': 'yarn install && pm2 startOrRestart ecosystem.config.js'
     }
   }
